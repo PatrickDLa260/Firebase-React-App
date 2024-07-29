5c597fbd03c7d474fe8ac108b3f9abc1f40ec573
PatrickDLa260 <patrick.la260@gmail.com>
2024-07-29 11:29:51 +1000

    Revert "android"
    This reverts commit a8ae8a444c560bbd88aa07d9e72c128b1b00137b.

--- src/utils/hooks/useNotificationPermission.ts
+++ src/utils/hooks/useNotificationPermission.ts
@@ -1,63 +1,22 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Platform, PermissionsAndroid } from 'react-native';
-import { requestNotifications, checkNotifications, RESULTS } from 'react-native-permissions';
 
 const useNotificationPermissionStatus = () => {
-    if (Platform.OS === 'web') {
-        const [permission, setPermission] = useState<NotificationPermission>("default");
+    const [permission, setPermission] = useState<NotificationPermission>("default");
 
-        useEffect(() => {
-            const handler = () => setPermission(Notification.permission);
-            handler();
-            Notification.requestPermission().then(handler);
+    useEffect(() => {
+        const handler = () => setPermission(Notification.permission);
+        handler();
+        Notification.requestPermission().then(handler);
 
-            navigator.permissions
-                .query({ name: "notifications" })
-                .then((notifactionPerm) => {
-                    notifactionPerm.onchange = handler;
-                });
-        }, []);
+        navigator.permissions
+            .query({ name: "notifications" })
+            .then((notifactionPerm) => {
+                notifactionPerm.onchange = handler;
+            });
+    }, []);
 
-        return permission;
-    }
-    else if (Platform.OS === 'android') {
-        try {
-            PermissionsAndroid.check('android.permission.POST_NOTIFICATIONS').then(
-                response => {
-                    if(!response){
-                        PermissionsAndroid.request('android.permission.POST_NOTIFICATIONS',{
-                            title: 'Notification',
-                            message:
-                            'App needs access to your notification ' +
-                            'so you can get Updates',
-                            buttonNeutral: 'Ask Me Later',
-                            buttonNegative: 'Cancel',
-                            buttonPositive: 'OK',
-                        })
-                    }
-                }
-            ).catch(
-              err => {
-                console.log("Notification Error=====>",err);
-              }
-            )
-          } catch (err){
-            console.log(err);
-          }
-    //else if (Platform.OS === 'ios' || Platform.OS === 'android') {
-        /* checkNotifications().then(({status, settings}) => {
-            if (status === RESULTS.UNAVAILABLE || status === RESULTS.DENIED || status === RESULTS.LIMITED || status === RESULTS.BLOCKED) {
-                requestNotifications(['alert', 'sound']).then(({status, settings}) => {
-
-                })
-            }
-            else if (status === RESULTS.GRANTED) {
-                status = "granted"
-            }
-            return status;
-        }) */
-    }
+    return permission;
 };
 
 export default useNotificationPermissionStatus;
